c9c686b160c438191c8d6d30887697724c09a272
alexamrojas007 <alexamrojas007@users.noreply.github.com>
2024-04-28 20:30:11 -0400

    Added basic form and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Love hurts but also heals",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
